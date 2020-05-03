5225e245d07c262b0e5e228e32f8e758bcc56fd7
akadateppei <62977964+akadateppei@users.noreply.github.com>
2020-05-03 20:52:20 +0900

    コマの内容

--- app/model.js
+++ app/model.js
@@ -1,4 +1,18 @@
 function makeboard() {
-    var 
+    //title: コマのタイトル, balance: コマによる収支, move: コマによる移動
+    var content = [{"title": "りゅーまにマックナゲットを一つとられた。ナゲット分お金が減る。", "balance": -50, "move": 0},
+        {"title": "化学豚モルモルにピタゴラスイッチを壊された。ピタゴラスイッチにかかった費用が無駄になる。", "balance": -100, "move":0},
+        {"title": "やけたいたたが拡散される。振り出しに戻る。", "balance": 0, "move": "start"},
+        {"title": "推薦で大学受かる。ゴール。", "balance": 0, "move": "goal"},
+        {"title": "応援団で大失敗。1,2,3...", "balance": -123, "move": 0},
+        {"title": "織姫が彼女になる。", "balance": 300, "move": 0},
+        {"title": "特に理由なく有井にブロックされる。腹立つ。", "balance": -100, "move": 0},
+        {"title": "彩乃のパンツ見えた。嬉しい。", "balance": 100, "move": 0},
+        {"title": "四天王にバンドに誘われる。", "balance": 300, "move": 0},
+        {"title": "ミカにふーたという彼氏がいた。", "balance": -200, "move": 0},
+        {"title": "えせすべに告白するも振られる。", "balance": -100, "move": 0},
+        {"title": "えせすべに存在を忘れられる。", "balance": -200, "move": 0},
+        {"title": "みーこに告白される", "balance": 1, "move": 0},
+        {"title": "ゆーくんにセフレがいたのを知る。", "balance": 0, "move": 0}];
 
 }
