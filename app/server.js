2448585cf653a81021a26edebea7739ff3d42682
akadateppei <62977964+akadateppei@users.noreply.github.com>
2020-05-04 23:42:29 +0900

    なんかいっぱいかえてしもた

--- app/server.js
+++ app/server.js
@@ -2,8 +2,12 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 //ここでただ一つのボードが作られる（全員共通）
+var board = require('./model.js');
 var playerList = [];
 var id = 0;
+var turn = 0;
+var numberOfPlayer = 0;
+var goaledPlayer = 0;
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
@@ -13,30 +17,56 @@ io.on('connection', (socket) => {
     //プレイヤー参加
     socket.on('player name', (playerName) => {
         id++;
-        var player = {playerId: "user" + id, player_name: playerName, socketId: socket.id};
+        numberOfPlayer++;
+        var player = {playerId: id, player_name: playerName, socketId: socket.id, move: 0, point: 0, goal: false};
         playerList.push(player);
         io.emit('player info', playerList);
-        if (id == 1){
-          io.emit('you are starter', "");
-        };
+        if (id == 1) {
+            io.emit('you are starter', "");
+        }
+        ;
         console.log(player);
     });
 
     // ゲームスタート
-    socket.on('game start', () =>{
-        // ボード初期化
-        var model = require('./model.js');
+    socket.on('game start', () => {
         //ゲームスタートを表示させる
         io.emit('display event', "game start" + '<br>' + playerList[0].player_name + 'の番です。');
         io.to(playerList[0].socketId).emit('your turn', "あなたの番です");
     });
 
-    socket.on('roll dice', (dice) =>{
-       console.log(dice);
-    });
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('roll dice', (dice) => {
+        console.log(turn);
+        console.log('numberofplayer' + numberOfPlayer);
+        playerList[turn].move += dice;
+        if (playerList[turn].move > 21){
+            goaledPlayer++;
+            playerList[turn].goal = true;
+            io.emit('display event', playerList[turn].player_name + 'がゴールしました。')
+        }else{
+            playerList[turn].point += board[playerList[turn].move].balance;
+            // マスによって分岐
+            switch (board[playerList[turn].move].move) {
+                case 0:
+                    io.emit('end turn', board[playerList[turn].move].title + "<br>" + "青春ポイント" + board[playerList[turn].move].balance);
+                    break;
+                case "start":
+                    playerList[turn].move = 0;
+                    break;
+                case "goal": // Todo: ゴール処理書いたら追加
+                    break;
+                default:
+                    io.emit('end turn', board[playerList[turn].move].title + "<br>" + "青春ポイント" + board[playerList[turn].move].balance + "<br>" + "移動" + board[playerList[turn].move].move);
+            }
+        }
+        io.emit('player info', playerList);
+        turn++
+        // ターン巻き戻し
+        if (turn === numberOfPlayer) {
+            turn = 0;
+        };
+        console.log(turn);
+        io.to(playerList[turn].socketId).emit('your turn', "あなたの番です");
     });
 
     socket.on('disconnect', () => {
