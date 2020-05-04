e9be4344909a529565e344b074ece315a8ddcff1
akadateppei <62977964+akadateppei@users.noreply.github.com>
2020-05-04 21:39:18 +0900

    スタートの処理おっけー

--- app/server.js
+++ app/server.js
@@ -2,7 +2,6 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 //ここでただ一つのボードが作られる（全員共通）
-var model = require('./model.js');
 var playerList = [];
 var id = 0;
 app.get('/', (req, res) => {
@@ -17,12 +16,16 @@ io.on('connection', (socket) => {
         var player = {playerId: "user" + id, player_name: playerName, socketId: socket.id};
         playerList.push(player);
         io.emit('player info', playerList);
-        console.log("user idは変更できてます:");
+        console.log(player);
     });
 
     // ゲームスタート
     socket.on('game start', () =>{
-
+        // ボード初期化
+        var model = require('./model.js');
+        //ゲームスタートを表示させる
+        io.emit('display event', "game start" + '<br>' + playerList[0].player_name + 'の番です。');
+        io.to(playerList[0].socketId).emit('your turn', "あなたの番です");
     });
 
     socket.on('roll dice', (dice) =>{
