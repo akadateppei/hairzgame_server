5ab58b7dea94faf742085da019eec6cd2949ae40
akadateppei <62977964+akadateppei@users.noreply.github.com>
2020-05-05 16:57:11 +0900

    落ちるバグ修正

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 //ここでただ一つのボードが作られる（全員共通）
 var board = require('./model.js');
+console.log(board);
 var playerList = [];
 var id = 0;
 var turn = 0;
@@ -18,7 +19,7 @@ io.on('connection', (socket) => {
     socket.on('player name', (playerName) => {
         id++;
         numberOfPlayer++;
-        var player = {playerId: id, player_name: playerName, socketId: socket.id, move: 0, point: 0, goal: false};
+        var player = {playerId: id, player_name: playerName, socketId: socket.id, move: 0, point: 0};
         playerList.push(player);
         io.emit('player info', playerList);
         if (id == 1) {
@@ -30,43 +31,65 @@ io.on('connection', (socket) => {
 
     // ゲームスタート
     socket.on('game start', () => {
+        playerList.forEach(function (value) {
+            value.point = 0;
+            value.move = 0;
+        });
+        turn = 0;
+        goaledPlayer = 0;
         //ゲームスタートを表示させる
         io.emit('display event', "game start" + '<br>' + playerList[0].player_name + 'の番です。');
         io.to(playerList[0].socketId).emit('your turn', "あなたの番です");
     });
 
     socket.on('roll dice', (dice) => {
-        console.log(turn);
-        console.log('numberofplayer' + numberOfPlayer);
-        playerList[turn].move += dice;
-        if (playerList[turn].move > 22){
-            goaledPlayer++;
-            playerList[turn].move = 23;
-            playerList[turn].goal = true;
-            io.emit('display event', playerList[turn].player_name + 'がゴールしました。')
-        }else{
-            playerList[turn].point += board[playerList[turn].move].balance;
-            // マスによって分岐
-            switch (board[playerList[turn].move].move) {
-                case 0:
-                    io.emit('end turn', board[playerList[turn].move].title + "<br>" + "青春ポイント" + board[playerList[turn].move].balance);
-                    break;
-                case "start":
-                    playerList[turn].move = 0;
-                    break;
-                case "goal": // Todo: ゴール処理書いたら追加
-                    break;
-                default:
-                    io.emit('end turn', board[playerList[turn].move].title + "<br>" + "青春ポイント" + board[playerList[turn].move].balance + "<br>" + "移動" + board[playerList[turn].move].move);
+        if (playerList[turn].move !== 23) { //ゴールしていない場合
+            playerList[turn].move += dice; //サイコロの目をmoveに足す
+            if (playerList[turn].move > 22) { //サイコロを足して22より大きくなった場合
+                goaledPlayer++;
+                playerList[turn].move = 23;
+                io.emit('display event', playerList[turn].player_name + 'がゴールしました。');
+            } else {
+                playerList[turn].point += board[playerList[turn].move].balance;
+                // マスによって分岐
+                switch (board[playerList[turn].move].move) {
+                    case 0:
+                        io.emit('end turn', board[playerList[turn].move].title + "<br>" + "青春ポイント" + board[playerList[turn].move].balance);
+                        break;
+                    case "start":
+                        io.emit('end turn', board[playerList[turn].move].title);
+                        playerList[turn].move = 0;
+                        break;
+                    case "goal":
+                        goaledPlayer++;
+                        io.emit('end turn', board[playerList[turn].move].title + "<br>" + "青春ポイント" + board[playerList[turn].move].balance +"<br>ゴールしました。");
+                        playerList[turn].move = 23;
+                        break;
+                    default:
+                        io.emit('end turn', board[playerList[turn].move].title + "<br>" + "青春ポイント" + board[playerList[turn].move].balance + "<br>" + "移動" + board[playerList[turn].move].move);
+                }
             }
+
         }
+        if (goaledPlayer === numberOfPlayer) {
+            var max = 0;
+            for (var i = 0; i === goaledPlayer - 1; i++) {
+                if (playerList[i].point > playerList[i + 1].point) {
+                    max = i;
+                } else {
+                    max = i + 1;
+                };
+            }
+            io.emit('game over', "ゲームオーバー！<br>最高の青春を送ったのは" + playerList[max].player_name + "です！");
+        };
         io.emit('player info', playerList);
+        console.log("turn: " + turn+"numberofplayer: "+ numberOfPlayer);
         turn++
         // ターン巻き戻し
-        if (turn === numberOfPlayer) {
+        if (turn == numberOfPlayer) {
             turn = 0;
-        };
-        console.log(turn);
+        }
+        ;
         io.to(playerList[turn].socketId).emit('your turn', "あなたの番です");
     });
 
